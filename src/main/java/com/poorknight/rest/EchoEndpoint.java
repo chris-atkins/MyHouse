581c8725e267d12129c6cb584cee318af1f95849
Chris Atkins <chrisatkins55@gmail.com>
2016-01-01 21:28:02 -0500

    new get endpoints for testing purposes to make sure the port-forwarding strategy for hue control will work before doing a real implementation

--- src/main/java/com/poorknight/rest/EchoEndpoint.java
+++ src/main/java/com/poorknight/rest/EchoEndpoint.java
@@ -8,8 +8,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.poorknight.echo.EchoRequestHandlerFactory;
 import com.poorknight.echo.EchoResponse;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 
 @Path("/")
 public class EchoEndpoint {
@@ -18,6 +23,31 @@ public class EchoEndpoint {
 	@Produces(MediaType.TEXT_PLAIN)
 	public String test() {
 		return "hi";
+	}
+
+	@GET
+	@Path("/on")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getOn() {
+		sendLightsRequestWithOn(true);
+		return "lights on";
+	}
+
+	@GET
+	@Path("/off")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getOff() {
+		sendLightsRequestWithOn(false);
+		return "ligths off";
+	}
+
+	private ClientResponse sendLightsRequestWithOn(final boolean lightsOn) {
+		final Client client = Client.create();
+		final WebResource webResource = client.resource("http://162.205.118.185:53335/api/6b1abf1f6e7157cc3843ee8b668d32d/lights/2/state");
+		final ObjectNode data = JsonNodeFactory.instance.objectNode();
+		data.set("on", JsonNodeFactory.instance.booleanNode(lightsOn));
+		final ClientResponse response = webResource.type("application/json").put(ClientResponse.class, data);
+		return response;
 	}
 
 	@POST
