bf29a36cabc6441fa807fd4a2f0dfd4cfce175fe
Chris Atkins <chrisatkins55@gmail.com>
2016-01-13 18:02:38 -0500

    reverting latest couple of changes

--- src/main/java/com/poorknight/rest/EchoEndpoint.java
+++ src/main/java/com/poorknight/rest/EchoEndpoint.java
@@ -3,7 +3,6 @@ package com.poorknight.rest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -56,12 +55,5 @@ public class EchoEndpoint {
 	@Produces(MediaType.APPLICATION_JSON)
 	public EchoResponse postEchoRequest(final JsonNode request) {
 		return EchoRequestHandlerFactory.handlerFor(request).handle();
-	}
-
-	@PUT
-	@Path("/hithere")
-	public String doRandomStuff() {
-
-		return "Hi there";
 	}
 }
