0b7be76abac00fc1ffe8e45a87641598f35f1d1d
Chris Atkins <chrisatkins55@gmail.com>
2017-10-01 17:59:29 -0400

    test to see if my alexa commands can stop other music that may be playing

---
+++ src/main/java/com/poorknight/echo/EchoDirective.java
@@ -0,0 +1,14 @@
+package com.poorknight.echo;
+
+public class EchoDirective {
+
+	private String type = "AudioPlayer.Stop";
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(final String type) {
+		this.type = type;
+	}
+}
