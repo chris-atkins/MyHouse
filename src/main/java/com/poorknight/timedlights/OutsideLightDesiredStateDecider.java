dd04e46054118e2abd7c3d4679868032051652ff
Chris <chrisatkins55@gmail.com>
2018-06-26 20:29:17 -0400

    Fix a bug in the light timer

--- src/main/java/com/poorknight/timedlights/OutsideLightDesiredStateDecider.java
+++ src/main/java/com/poorknight/timedlights/OutsideLightDesiredStateDecider.java
@@ -57,6 +57,6 @@ public class OutsideLightDesiredStateDecider {
 
 		int fiveHours = 18000000;
 		int offset = zone.getOffset(utcNow) + fiveHours;
-		return utcNow.plus(offset);
+		return utcNow.minus(offset);
 	}
 }
