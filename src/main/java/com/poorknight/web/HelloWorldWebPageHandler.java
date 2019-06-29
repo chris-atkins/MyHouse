ac3d6554db302759073970a492c3dcd46a4b4395
Chris <chrisatkins55@gmail.com>
2019-06-29 14:32:45 -0400

    Add logback as logging framework

--- src/main/java/com/poorknight/web/HelloWorldWebPageHandler.java
+++ src/main/java/com/poorknight/web/HelloWorldWebPageHandler.java
@@ -8,12 +8,18 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HelloWorldWebPageHandler extends AbstractHandler {
-	
+
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorldWebPageHandler.class);
+
 	@Override
 	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
+
+		logger.info("Serving response for GET request at {}", "/web/hello");
 
 		response.setContentType("text/html;charset=utf-8");
 		response.setStatus(HttpServletResponse.SC_OK);
