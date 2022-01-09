6df3de8957417434893df788930c269a0bbefdc1
Chris <chrisatkins55@gmail.com>
2022-01-09 16:06:50 -0500

    Remove unused imports

--- src/main/java/com/poorknight/scheduledtasks/timedlights/PlantLightsDesiredStateDecider.java
+++ src/main/java/com/poorknight/scheduledtasks/timedlights/PlantLightsDesiredStateDecider.java
@@ -1,16 +1,10 @@
 package com.poorknight.scheduledtasks.timedlights;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.sun.jersey.api.client.Client;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
-import org.joda.time.Days;
-
-import javax.ws.rs.core.MediaType;
 
 import static com.poorknight.scheduledtasks.timedlights.DesiredState.OFF;
 import static com.poorknight.scheduledtasks.timedlights.DesiredState.ON;
-import static org.joda.time.DateTimeZone.UTC;
 
 public class PlantLightsDesiredStateDecider {
 
